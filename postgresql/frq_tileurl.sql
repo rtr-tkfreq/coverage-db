af19adc39957ad8f0eb53de0bc209723549d8580
Dietmar Zlabinger <dietmar@zlabinger.at>
2022-02-19 14:02:31 +0000

    Update tileurl

--- postgresql/frq_tileurl.sql
+++ postgresql/frq_tileurl.sql
@@ -86,3 +86,46 @@ SELECT pg_catalog.setval('public.tileurl_uid_seq', 4, true);
 -- PostgreSQL database dump complete
 --
 
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.5 (Debian 13.5-0+deb11u1)
+-- Dumped by pg_dump version 13.5 (Debian 13.5-0+deb11u1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: tileurl; Type: VIEW; Schema: api; Owner: postgres
+--
+
+CREATE VIEW api.tileurl AS
+ SELECT tileurl.operator,
+    tileurl.reference,
+    tileurl.date,
+    tileurl.url
+   FROM public.tileurl;
+
+
+ALTER TABLE api.tileurl OWNER TO postgres;
+
+--
+-- Name: TABLE tileurl; Type: ACL; Schema: api; Owner: postgres
+--
+
+GRANT SELECT ON TABLE api.tileurl TO web_anon;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
